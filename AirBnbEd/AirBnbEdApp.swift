2b76148db443535b314a248de92aa2d9ad598e84
vbugrym <vbugrym@lohika.com>
2023-10-30 15:12:30 +0200

    Initial Commit

---
+++ AirBnbEd/AirBnbEdApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbEdApp.swift
+//  AirBnbEd
+//
+//  Created by vbugrym on 30.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbEdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
