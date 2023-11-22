49e46e130a066fd0be5d4c8381e8fcbc670ed13e
vbugrym <vbugrym@lohika.com>
2023-11-22 23:14:39 +0200

    Added search view with animation for different states

--- AirBnbEd/Core/Explore/ExploreView.swift
+++ AirBnbEd/Core/Explore/ExploreView.swift
@@ -8,28 +8,41 @@
 import SwiftUI
 
 struct ExploreView: View {
+    
+    @State private var showDestinationSearchView = false
+    
     var body: some View {
         //lets navigate to another views
         NavigationStack {
-            ScrollView {
-                SearchAndFilterBar()
-                
-                //lazy stack renders only onscreen views
-                LazyVStack(spacing: 32) {
-                    ForEach(1...10, id:\.self) { listing in
-                        
-                        NavigationLink(value: listing) {
-                            ListingItemView()
-                                .frame(height: 400)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            if showDestinationSearchView {
+                DestinationSearchView(show: $showDestinationSearchView)
+            } else {
+                ScrollView {
+                    SearchAndFilterBar()
+                        .onTapGesture {
+                            withAnimation(.snappy) {
+                                showDestinationSearchView.toggle()
+                            }
+                        }
+                    
+                    //lazy stack renders only onscreen views
+                    LazyVStack(spacing: 32) {
+                        ForEach(1...10, id:\.self) { listing in
+                            
+                            NavigationLink(value: listing) {
+                                ListingItemView()
+                                    .frame(height: 400)
+                                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                            }
                         }
                     }
+                    .padding()
                 }
-                .padding()
-            }
-            .navigationDestination(for: Int.self) { listing in
-                ListingDetailView()
-                    .navigationBarBackButtonHidden()
+                .navigationDestination(for: Int.self) { listing in
+                    ListingDetailView()
+                        .navigationBarBackButtonHidden()
+                }
             }
         }
     }
