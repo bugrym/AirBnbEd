5bb83d338db9dbacb1ee551398c9dcba76e8dd37
vbugrym <vbugrym@lohika.com>
2023-11-29 09:48:04 +0200

    Clean code

--- AirBnbEd/Core/Wishlists/WishlistsView.swift
+++ AirBnbEd/Core/Wishlists/WishlistsView.swift
@@ -11,37 +11,20 @@ struct WishlistsView: View {
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading, spacing: 32) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Log in to view your wishlists")
+                        .font(.headline)
+                    
+                    Text("You can create, view or edit wishlists once you've logged in")
+                        .font(.footnote)
+                }
                 
+                LogInButton() {}
                 
-                Text("$ 100")
-                    .frame(width: 150, height: 50)
-                    .fontWeight(.semibold)
-                    .font(.title)
-                    .foregroundStyle(.white)
-                    .background(.gray)
-                    .clipShape(RoundedRectangle(cornerRadius: 25))
-                    .overlay {
-                        Capsule()
-                            .stroke(lineWidth: 0.5)
-                            .foregroundStyle(Color(.black))
-                    }
-                    .shadow(color: .black, radius: 2, y: 2)
-                
-                
-//                VStack(alignment: .leading, spacing: 4) {
-//                    Text("Log in to view your wishlists")
-//                        .font(.headline)
-//                    
-//                    Text("You can create, view or edit wishlists once you've logged in")
-//                        .font(.footnote)
-//                }
-//                
-//                LogInButton() {}
-//                
-//                Spacer()
+                Spacer()
             }
-//            .padding()
-//            .navigationTitle("Wishlists")
+            .padding()
+            .navigationTitle("Wishlists")
         }
         
     }
